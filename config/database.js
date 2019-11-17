11a573494607fdd51b72f1466d2ae157a0af08a4
Resilient Coders 7 <mauricio.acosta92@gmail.com>
2019-11-17 18:59:36 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://demoDay:demoDay@cluster0-rnihr.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'demoDayProject'
+};
